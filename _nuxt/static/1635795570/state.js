7408f16cb08bd67ab4ad4624ee31c43c8cf7d388
omiqa-dev <omiqa-dev@users.noreply.github.com>
2021-11-01 19:46:04 +0000

    deploy: 51cb64a82fd4024ab1a173eba8cbb54d5f660940

--- _nuxt/static/1635795570/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmedvinsky-app\u002F_nuxt\u002Fstatic\u002F1635795570",layout:"default",error:a,state:{dataset:"LCM-Seq CS13",ligand:a,receptor:a},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmedvinsky-app\u002F",assetsPath:"\u002Fmedvinsky-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
