089c8742bf58b585811feed5d9e36516f670e207
omiqa-dev <accounts@omiqa.bio>
2021-11-25 14:58:35 +0100

    chore: added standard button component

---
+++ components/ui/StandardButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    :class="{ 'bg-yellow-400': hover, 'text-gray-900': hover }"
+    class="p-1.5 bg-gray-900 text-gray-50 rounded w-28"
+    @mouseenter="hover = true"
+    @mouseleave="hover = false"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({
+    hover: false,
+  }),
+};
+</script>
