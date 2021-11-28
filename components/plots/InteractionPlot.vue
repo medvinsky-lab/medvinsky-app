d07ee016d1bb7ed367acf034d76423c955d21ed5
omiqa-dev <accounts@omiqa.bio>
2021-11-28 10:43:22 +0100

    chore: added d3

--- components/plots/InteractionPlot.vue
+++ components/plots/InteractionPlot.vue
@@ -1,3 +1,32 @@
 <template>
-  <p>This is an interaction plot</p>
+  <div class="flex-grow flex flex-col">
+    <div id="chart" ref="chart" class="bg-white flex-grow rounded"></div>
+  </div>
 </template>
+
+<script>
+import * as d3 from 'd3';
+
+export default {
+  mounted() {
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .attr('viewBox', `0 0 620 578`);
+
+    svg
+      .append('circle')
+      .attr('cx', 50)
+      .attr('cy', 50)
+      .attr('r', 50)
+      .attr('stroke-width', 1);
+  },
+  data: () => ({
+    chartData: [40, 60, 80, 100, 70, 120, 100, 60, 70, 150, 120, 140],
+    barWidth: 40,
+    barOffset: 10,
+  }),
+};
+</script>
