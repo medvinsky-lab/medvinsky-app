6724f2d1eba375bb10bce4886013842e39343197
omiqa-dev <accounts@omiqa.bio>
2022-01-07 13:04:59 +0100

    feat: added umap plots

---
+++ components/plots/ScatterPlot.vue
@@ -0,0 +1,88 @@
+<template>
+  <highchart :options="chartOptions" />
+</template>
+
+<script>
+export default {
+  props: {
+    plotData: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: 'Title',
+    },
+    xAxisTitle: {
+      type: String,
+      default: 'X',
+    },
+    yAxisTitle: {
+      type: String,
+      default: 'Y',
+    },
+  },
+  computed: {
+    chartOptions() {
+      return {
+        chart: {
+          type: 'scatter',
+          zoomType: 'xy',
+        },
+        title: {
+          text: this.title,
+        },
+        xAxis: {
+          title: {
+            enabled: true,
+            text: this.xAxisTitle,
+          },
+          lineWidth: 0,
+          tickWidth: 0,
+          labels: {
+            enabled: false,
+          },
+        },
+        yAxis: {
+          title: {
+            enabled: true,
+            text: this.yAxisTitle,
+          },
+          gridLineWidth: 0,
+          labels: {
+            enabled: false,
+          },
+        },
+        plotOptions: {
+          scatter: {
+            marker: {
+              symbol: 'circle',
+              radius: 2,
+              states: {
+                hover: {
+                  enabled: true,
+                  lineColor: 'rgb(100,100,100)',
+                },
+              },
+            },
+            states: {
+              hover: {
+                marker: {
+                  enabled: false,
+                },
+              },
+            },
+            tooltip: {
+              headerFormat: '<b>{series.name}</b><br>',
+            },
+          },
+        },
+        series: this.plotData,
+        credits: {
+          enabled: false,
+        },
+      };
+    },
+  },
+};
+</script>
---
+++ components/visuals/Umap.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex-grow">
+    <scatter-plot
+      class="mb-4 rounded"
+      :plot-data="umap1Data"
+      title="Hematoendothelial"
+      x-axis-title="UMAP1"
+      y-axis-title="UMAP2"
+    />
+    <scatter-plot
+      class="rounded"
+      :plot-data="umap2Data"
+      title="Mesenchymal/epithelial"
+      x-axis-title="UMAP1"
+      y-axis-title="UMAP2"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    umap1Data: null,
+    umap2Data: null,
+  }),
+  async fetch() {
+    const umap1Data = await this.$content('data/umap')
+      .where({ slug: 'umap_he1' })
+      .fetch();
+    const umap2Data = await this.$content('data/umap')
+      .where({ slug: 'umap_niche2' })
+      .fetch();
+    this.umap1Data = umap1Data;
+    this.umap2Data = umap2Data;
+  },
+};
+</script>
